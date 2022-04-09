b327fe8add817eae71c525b0e0568d9a5c78cc19
Thawany22 <souzaathaay4@gmail.com>
2022-04-08 21:40:43 -0300

    thawany22

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sinal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            
+            Console.Write("Digite um número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                Console.WriteLine("Negativo");
+            }
+            else if (numero > 0)
+            {
+                Console.WriteLine("Positivo");
+            }
+            else
+            {
+                Console.WriteLine("Zero");
+            }
+        }
+    }
+}
